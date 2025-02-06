821363da8ca893c91dbb20b2fb23282129a6f356
Christopher Kiessling <Crsk@mail.com>
2025-02-06 14:38:34 -0300

    feat: allow non logged-in users to view partners

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,13 @@
 'use client'
 import { Button } from '@/components/ui/button'
-import { useAuth } from '@/contexts/auth/auth.context'
 import { QrCode, UsersIcon } from 'lucide-react'
 import Link from 'next/link'
 
 export default function Home() {
-  const { authUser } = useAuth()
-
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen font-[family-name:var(--font-geist-sans)] pb-36">
       <main className="flex flex-col gap-8 row-start-2 items-center justify-center">
-        <h1 className="text-3xl pb-20">Control de Acceso</h1>
+        <h1 className="text-3xl pb-16">Control de Acceso</h1>
         <div className="flex gap-4">
           <Link href="/scan">
             <Button size="lg">
@@ -18,13 +15,11 @@ export default function Home() {
             </Button>
           </Link>
 
-          {authUser?.email && (
-            <Link href="/socios">
-              <Button size="lg" variant="secondary">
-                <UsersIcon /> Administrar Socios
-              </Button>
-            </Link>
-          )}
+          <Link href="/socios">
+            <Button size="lg" variant="secondary">
+              <UsersIcon /> Socios
+            </Button>
+          </Link>
         </div>
       </main>
       <footer></footer>
