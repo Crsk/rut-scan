f23ac11d9ea42aaef7610a9458dde3b315b2082e
Christopher Kiessling <Crsk@mail.com>
2025-02-06 10:20:03 -0300

    feat: improve home layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 'use client'
 import { Button } from '@/components/ui/button'
 import { useAuth } from '@/contexts/auth/auth.context'
+import { QrCode, UsersIcon } from 'lucide-react'
 import Link from 'next/link'
 
 export default function Home() {
@@ -9,16 +10,20 @@ export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen font-[family-name:var(--font-geist-sans)] pb-36">
       <main className="flex flex-col gap-8 row-start-2 items-center justify-center">
-        <h1 className="text-3xl">Control de Acceso</h1>
+        <h1 className="text-3xl pb-20">Control de Acceso</h1>
         <div className="flex gap-4">
           <Link href="/scan">
-            <Button size="lg">Validar QR</Button>
+            <Button size="lg">
+              <QrCode /> Validar QR
+            </Button>
           </Link>
 
           {authUser?.email && (
-            <Button size="lg" variant="secondary">
-              <Link href="/socios">Administrar Socios</Link>
-            </Button>
+            <Link href="/socios">
+              <Button size="lg" variant="secondary">
+                <UsersIcon /> Administrar Socios
+              </Button>
+            </Link>
           )}
         </div>
       </main>
