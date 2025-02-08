85d2ab3273d85868ad2430208de34869bf0bd942
Christopher Kiessling <Crsk@mail.com>
2025-02-08 13:13:40 -0300

    feat: update camera plugin, enhance permission request and add overlay animation

--- src/app/scan/page.tsx
+++ src/app/scan/page.tsx
@@ -1,9 +1,9 @@
 'use client'
 import { useCallback, useEffect, useState } from 'react'
-import { Scanner } from '@yudiel/react-qr-scanner'
 import { getRunFromUrl } from './_utils/get-run-from-url'
 import { useUserByRut } from '@/domain/users/hooks/use-user-by-rut'
 import { useRouter } from 'next/navigation'
+import QRScanner from '@/app/scan/_components/scanner'
 
 const Page = () => {
   const [rut, setRut] = useState<string>('')
@@ -27,7 +27,7 @@ const Page = () => {
 
   return (
     <div className="pt-20">
-      <Scanner onScan={result => onNewScanResult(result[0].rawValue)} />
+      <QRScanner onScan={result => result && onNewScanResult(result)} />
     </div>
   )
 }
