a813a90fc0f63d645853dac45fe64a851604adb4
Christopher Kiessling <Crsk@mail.com>
2025-02-09 15:04:55 -0300

    feat: correct recognition marks (build attempt)

--- src/app/face-recognition/_components/face-recognition.tsx
+++ src/app/face-recognition/_components/face-recognition.tsx
@@ -166,17 +166,17 @@ export const FaceRecognition: FC<DetectionProps> = ({ referenceImages }) => {
   }, [isModelLoaded, referenceDescriptors, detectFaces])
 
   return (
-    <div className="relative h-[100vw] w-[100vw]">
+    <div className="relative h-auto w-[100vw]">
       <video
         ref={videoRef}
         autoPlay
         muted
         playsInline
         onPlay={detectFaces}
-        className="h-[100vw] w-[100vw]"
+        className="h-auto w-[100vw]"
         style={{ objectFit: 'cover' }}
       />
-      <canvas ref={canvasRef} className="absolute top-0 left-0 h-[100vw] w-[100vw]" />
+      <canvas ref={canvasRef} className="absolute top-0 left-0 h-auto w-[100vw]" />
 
       <div className="absolute">
         {!isModelLoaded && (
